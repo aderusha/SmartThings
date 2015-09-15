fa757d7d56aca0b0d15a6c0992e27b054f4e2cb2
aderusha <Allen@Thorium.derusha.net>
2015-09-15 11:25:54 -0400

    Updated to 1.1.0
    Changed handling of binary(ish) vs non-binary values to allow sending
    value data to EG to be handled via Python and eg.event.payload[]

--- Send-Events-to-EventGhost.groovy
+++ Send-Events-to-EventGhost.groovy
@@ -6,6 +6,8 @@
  *  https://github.com/aderusha/SmartThings/blob/master/Send-Events-to-EventGhost.groovy
  *  Copyright 2015 aderusha
  *  Version 1.0.0 - 2015-09-13 - Initial release
+ *  Version 1.1.0 - 2015-09-15 - Changed handling of binary(ish) vs non-binary values to allow sending
+ *                               value data to EG to be handled via Python and eg.event.payload[]
  *
  *  This SmartApp will send selected events to an EventGhost server running the Webserver plugin.
  *  EventGhost is a Windows application used for event automation, find out more here: http://www.eventghost.org/
@@ -51,7 +53,7 @@ preferences {
 		input "myDimmer", "capability.switchLevel", title: "Dimmers", required: false, multiple: true
 		input "myColorControl", "capability.colorControl", title: "Color Controls", required: false, multiple: true
 		input "myButton", "capability.button", title: "Buttons", required: false, multiple: true
-		input "myMomentaryContact", "capability.momentary", title: "Momentary Contact Switch", required: false, multiple: true
+		input "myMomentaryContact", "capability.momentary", title: "Momentary Contacts", required: false, multiple: true
 		input "myMotion", "capability.motionSensor", title: "Motion Sensors", required: false, multiple: true
 		input "myContact", "capability.contactSensor", title: "Contact Sensors", required: false, multiple: true
 		input "myLock", "capability.lock", title: "Locks", required: false, multiple: true
@@ -65,7 +67,7 @@ preferences {
 		input "myPresence", "capability.presenceSensor", title: "Presence Sensors", required: false, multiple: true
 		input "mySmoke", "capability.smokeDetector", title: "Smoke Sensors", required: false, multiple: true
 		input "myWater", "capability.waterSensor", title: "Water Sensors", required: false, multiple: true
-		input "myCO", "capability.carbonMonoxideDetector", title: "Carbon Monoxide Detector", required: false, multiple: true
+		input "myCO", "capability.carbonMonoxideDetector", title: "Carbon Monoxide Detectors", required: false, multiple: true
 	}
 }
 
@@ -81,32 +83,48 @@ def updated() {
 }
 
 def subscribeToEvents() {
-	subscribe(mySwitch, "switch", eventHandler)
-	subscribe(myDimmer, "level", eventHandler)
-	subscribe(myColorControl, "color", eventHandler)
-	subscribe(myButton, "button", eventHandler)
-	subscribe(myMomentaryContact, "momentary", eventHandler)
-	subscribe(myMotion, "motion", eventHandler)
-	subscribe(myContact, "contact", eventHandler)
-	subscribe(myLock, "lock", eventHandler)
-	subscribe(myThermostat, "thermostat", eventHandler)
-	subscribe(myTemperature, "temperature", eventHandler)
-	subscribe(myBrightness, "illuminance", eventHandler)
-	subscribe(myHumidty, "humidity", eventHandler)
-	subscribe(myEnergy, "energy", eventHandler)
-	subscribe(myPower, "power", eventHandler)
-	subscribe(myAcceleration, "acceleration", eventHandler)
-	subscribe(myPresence, "presence", eventHandler)
-	subscribe(mySmoke, "smoke", eventHandler)
-	subscribe(myWater, "water", eventHandler)
-	subscribe(myCO, "carbonMonoxide", eventHandler)
+	subscribe(mySwitch, "switch", eventHandlerBinary)
+	subscribe(myDimmer, "level", eventHandlerValue)
+	subscribe(myColorControl, "color", eventHandlerValue)
+	subscribe(myButton, "button", eventHandlerBinary)
+	subscribe(myMomentaryContact, "momentary", eventHandlerBinary)
+	subscribe(myMotion, "motion", eventHandlerBinary)
+	subscribe(myContact, "contact", eventHandlerBinary)
+	subscribe(myLock, "lock", eventHandlerBinary)
+	subscribe(myThermostat, "thermostat.thermostatMode", eventHandlerBinary)
+	subscribe(myThermostat, "thermostat.thermostatFanMode", eventHandlerBinary)
+	subscribe(myThermostat, "thermostat.thermostatOperatingState", eventHandlerBinary)
+	subscribe(myThermostat, "thermostat.temperature", eventHandlerValue)
+	subscribe(myThermostat, "thermostat.heatingSetpoint", eventHandlerValue)
+	subscribe(myThermostat, "thermostat.coolingSetpoint", eventHandlerValue)
+	subscribe(myThermostat, "thermostat.thermostatSetpoint", eventHandlerValue)
+	subscribe(myTemperature, "temperature", eventHandlerValue)
+	subscribe(myBrightness, "illuminance", eventHandlerValue)
+	subscribe(myHumidty, "humidity", eventHandlerValue)
+	subscribe(myEnergy, "energy", eventHandlerValue)
+	subscribe(myPower, "power", eventHandlerValue)
+	subscribe(myAcceleration, "acceleration", eventHandlerBinary)
+	subscribe(myPresence, "presence", eventHandlerBinary)
+	subscribe(mySmoke, "smoke", eventHandlerBinary)
+	subscribe(myWater, "water", eventHandlerBinary)
+	subscribe(myCO, "carbonMonoxide", eventHandlerBinary)
 }
 
-def eventHandler(evt) {
+def eventHandlerBinary(evt) {
 	def egHost = "${settings.egServer}:${settings.egPort}"
 	def egRawCommand = "${settings.egPrefix}.${evt.displayName}.${evt.name}.${evt.value}"
 	def egRestCommand = java.net.URLEncoder.encode(egRawCommand)
 	log.debug "processed event ${evt.name} from device ${evt.displayName} with value ${evt.value}"
 	log.debug "egRestCommand:  $egRestCommand"
 	sendHubCommand(new physicalgraph.device.HubAction("""GET /?$egRestCommand HTTP/1.1\r\nHOST: $egHost\r\n\r\n""", physicalgraph.device.Protocol.LAN))
+}
+
+def eventHandlerValue(evt) {
+	def egHost = "${settings.egServer}:${settings.egPort}"
+	def egRawCommand = "${settings.egPrefix}.${evt.displayName}.${evt.name}"
+	def egRestCommand = java.net.URLEncoder.encode(egRawCommand)
+	def egRestCommandValue = "$egRestCommand&${evt.value}"
+	log.debug "processed event ${evt.name} from device ${evt.displayName} with value ${evt.value}"
+	log.debug "egRestCommand:  $egRestCommandValue"
+	sendHubCommand(new physicalgraph.device.HubAction("""GET /?$egRestCommandValue HTTP/1.1\r\nHOST: $egHost\r\n\r\n""", physicalgraph.device.Protocol.LAN))
 }
